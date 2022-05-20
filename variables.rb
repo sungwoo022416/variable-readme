009a08546f75c16cd6183e284782922fade8291a
Sungwoo Kim <sungwoo.kim@yale.edu>
2022-05-20 09:36:29 -0700

    Done with assignment

--- variables.rb
+++ variables.rb
@@ -13,3 +13,29 @@ puts example
 
 puts "variables are any previously undefined word that"
 puts "starts with a lowercase letter."
+
+first_number = 7
+second_number = 14
+
+sum = first_number + second_number
+
+puts sum
+
+current_president = "Barack Obama"
+puts "In 2016, the president was #{current_president}." 
+
+current_president = "Barack Obama"
+puts "In 2016, the president was #{current_president}."
+
+current_president = "Stephen Colbert"
+puts "Now, it being the year 2017, the president is #{current_president}." 
+
+# Open up IRB and follow along
+sound = "squeak"
+
+# We can peek at the value of sound by typing its name
+sound
+# => "squeak"
+
+sound.upcase
+# => "SQUEAK"
